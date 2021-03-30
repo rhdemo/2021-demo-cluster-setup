75c5decb45736223f3cff5486766340c57224e0d
Evan Shortiss <evanshortiss@gmail.com>
2021-03-30 11:39:22 +0100

    fix: default cluster name/env and memory/cpu for socket server

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -3,7 +3,7 @@
 printf "\n\n######## frontend/deploy ########\n"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT=${PROJECT:-frontend}
-CLUSTER_NAME=${CLUSTER_NAME:-EDGE}
+CLUSTER_NAME=${CLUSTER_NAME:-Default}
 ROLLOUT_STRATEGY=${ROLLOUT_STRATEGY:-Rolling}
 
 oc project ${PROJECT} 2> /dev/null || oc new-project ${PROJECT}
