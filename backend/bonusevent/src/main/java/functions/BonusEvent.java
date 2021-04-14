a34092eb96b47544e572b0aab422d52a135db6e5
utherp0 <ian.lawson@redhat.com>
2021-04-14 13:26:44 +0100

    Updated bonus to 1.13.1 and changed from uni to non-async

--- backend/bonusevent/src/main/java/functions/BonusEvent.java
+++ backend/bonusevent/src/main/java/functions/BonusEvent.java
@@ -4,6 +4,7 @@ import io.quarkus.funqy.Context;
 import io.quarkus.funqy.Funq;
 import io.quarkus.funqy.knative.events.CloudEvent;
 import io.quarkus.funqy.knative.events.CloudEventMapping;
+import io.quarkus.funqy.knative.events.CloudEventBuilder;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.subscription.UniEmitter;
 import io.vertx.core.Vertx;
@@ -36,22 +37,20 @@ public class BonusEvent
     String _prodmode;
 
     @Funq
-    @CloudEventMapping(responseType = "bonusprocessed")
-    //public Uni<MessageOutput> function( Input input, @Context CloudEvent cloudEvent)
-    public Uni<MessageOutput> function( String input, @Context CloudEvent cloudEvent)
+    public CloudEvent<MessageOutput> processor( String input )  
     {
-      return Uni.createFrom().emitter(emitter -> 
-      {
-        buildResponse(input, cloudEvent, emitter);
-      });    
+      MessageOutput output = buildResponse( input );
+      String eventName = ( output.getHostname() == null ? "attackprocessed" : "attackprocessed-" + output.getHostname() );
+
+      return CloudEventBuilder.create()
+        .type(eventName)
+        .build(output);      
     }
- 
-    public void buildResponse( String input, CloudEvent cloudEvent, UniEmitter<? super MessageOutput> emitter )
+
+    public MessageOutput buildResponse( String input )
     {
       // Setup Watchman
       Watchman watchman = new Watchman( _watchmanURL );
-
-      System.out.println("Bonus Event Received..." );
 
       //Process the payload
       try
@@ -65,6 +64,7 @@ public class BonusEvent
         String match = message.getString("match");
         JsonObject by = message.getJsonObject("by");
         String uuid = by.getString("uuid");
+        String hostname = message.getString("hostname");
         Long ts = message.getLong("ts");
         boolean human = by.getBoolean("human");
         String username = by.getString("username");
@@ -76,57 +76,46 @@ public class BonusEvent
           LocalDateTime now = LocalDateTime.now();
 
           boolean watched = watchman.inform( "[BONUS] (" + now.toString() +"):" + match + " game:" + game + " uuid: " + uuid + " name: " + username + ( shots != null ? " shots:" + shots.toString() : "" ));
-        }
 
-        // Log for verbosity :-) 
-        System.out.println( "  Game: " + game );
-        System.out.println( "  Match: " + match );
-        System.out.println( "  UUID: " + uuid );
-        System.out.println( "  Username: " + username );
-        System.out.println( "  Human: " + human );
+          // Log for verbosity :-) 
+          System.out.println( "  Game: " + game );
+          System.out.println( "  Match: " + match );
+          System.out.println( "  UUID: " + uuid );
+          System.out.println( "  Username: " + username );
+          System.out.println( "  Human: " + human );
+        }
 
         String envValue = System.getenv("BONUS_SCORE");
         int delta = 0;
         int multiplier = ( envValue == null ? DEFAULT_BONUS_SCORE : Integer.parseInt(envValue));
         delta = ( shots == null ? multiplier : ( shots * multiplier));
 
-        if( shots != null )
+        output.setGame(game);
+        output.setMatch(match);
+        output.setUuid(uuid);
+        output.setHostname(hostname);
+        output.setTs(ts);
+        output.setDelta(Integer.valueOf(delta));
+        output.setHuman(human);
+
+        // Convert spaces in the username for URL
+        username = username.replaceAll(" ", "%20");
+
+        // Post to Scoring Service
+        String compositePostURL = _scoringServiceURL + "scoring/" + game + "/" + match + "/" + uuid + "?delta=" + delta + "&human=" + human + "&username" + username + "&timestamp=" + ts + "&bonus=true";
+
+        Postman postman = new Postman( compositePostURL );
+        if( !( postman.deliver("dummy")))
         {
-          System.out.println( "  Shots (multiple):" + shots.toString());
-        }
-        else
-        {
-          System.out.println( "  Single Bonus Shot." );
-          delta = 1;
+          System.out.println( "Failed to update Scoring Service");
         }
 
-          output.setGame(game);
-          output.setMatch(match);
-          output.setUuid(uuid);
-          output.setTs(ts);
-          output.setDelta(Integer.valueOf(delta));
-          output.setHuman(human);
-
-          // Convert spaces in the username for URL
-          username = username.replaceAll(" ", "%20");
-
-          // Post to Scoring Service
-          String compositePostURL = _scoringServiceURL + "scoring/" + game + "/" + match + "/" + uuid + "?delta=" + delta + "&human=" + human + "&username" + username + "&timestamp=" + ts + "&bonus=true";
-
-          Postman postman = new Postman( compositePostURL );
-          if( !( postman.deliver("dummy")))
-          {
-            System.out.println( "Failed to update Scoring Service");
-          }
-
-          // Post hit to SHOTS scoring-service
-
-          emitter.complete(output);
-        }
-        catch( Exception exc )
-        {
-          System.out.println("Failed to parse JSON due to " + exc.toString());
-          return;  
-        }
+        return output;
+      }
+      catch( Exception exc )
+      {
+        System.out.println("Failed to parse JSON due to " + exc.toString());
+        return null;  
+      }
     }
 }
