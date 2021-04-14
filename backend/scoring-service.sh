fe0fd405ba3d7918efc5ab30a1a292a46cc40348
utherp0 <ian.lawson@redhat.com>
2021-04-14 15:22:08 +0100

    And the scoring service...

--- backend/scoring-service.sh
+++ backend/scoring-service.sh
@@ -1,4 +1,4 @@
 oc project battleships-scoring 2> /dev/null || oc new-project battleships-scoring
 
 oc new-app quay.io/redhatdemo/2021-scoring-service --name="scoring-service" --as-deployment-config=true --env="QUARKUS_INFINISPAN_CLIENT_SERVER_LIST=datagrid.datagrid.svc.cluster.local:11222"
-oc apply -f scoring-route.yaml
+oc apply -f backend/scoring-route.yaml
