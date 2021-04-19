3ef66ab63917fecaf527a13d032931b96970adb4
Evan Shortiss <evanshortiss@gmail.com>
2021-04-19 11:33:49 +0100

    fix: use external leaderboard route

--- dashboard/deploy.sh
+++ dashboard/deploy.sh
@@ -6,8 +6,11 @@ PROJECT=${PROJECT:-battleships-dashboard}
 
 oc project ${PROJECT} 2> /dev/null || oc new-project ${PROJECT}
 
+LEADERBOARD_ROUTE=$(oc get route leaderboard -n battleships-leaderboard -o jsonpath='{.spec.host}')
+
 # Apply knative triggers in backend to forward score updates to the game server
 oc process -f "${DIR}/template.yml" \
--p NAMESPACE="${PROJECT}" \ 
+-p LEADERBOARD_SERVER="${LEADERBOARD_ROUTE}" \
+-p STATS_SERVER="${LEADERBOARD_ROUTE}" \
 -p REPLAY_SERVER="${DASHBOARD_REPLAY_SERVER}" \
 -p GAME_SERVER="${DASHBOARD_GAME_SERVER}" | oc create -n $PROJECT -f -
