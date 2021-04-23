007ef722cf6ddc9d656d9bc36f16fd76744e9c7c
utherp0 <ian.lawson@redhat.com>
2021-04-23 13:21:01 +0100

    Moved triggers out of source directories in advance of removal of source, updated deploy.sh to reflect new location

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -9,10 +9,10 @@ printf "Creating broker..."
 oc apply -f $DIR/broker.yaml
 
 printf "Creating triggers..."
-oc apply -f $DIR/attackevent/trigger-attack.yaml
-oc apply -f $DIR/bonusevent/trigger-bonus.yaml
-oc apply -f $DIR/matchend/trigger-match-end.yaml
-oc apply -f $DIR/matchstart/trigger-match-start.yaml
+oc apply -f $DIR/trigger-attack.yaml
+oc apply -f $DIR/trigger-bonus.yaml
+oc apply -f $DIR/trigger-match-end.yaml
+oc apply -f $DIR/trigger-match-start.yaml
 
 printf "Creating functions"
 kn service create attack --image quay.io/redhatdemo/2021-backend-attack --env SCORINGSERVICE=http://scoring-service.battleships-scoring.svc.cluster.local:8080/ --env WATCHMAN=http://watchman.battleships-backend.svc.cluster.local:8080/watch --env CARRIER_SCORE=100 --env DESTROYER_SCORE=250 --env SUBMARINE_SCORE=150 --env BATTLESHIP_SCORE=200 --env HIT_SCORE=5 --env PRODMODE=production --env NAMESPACE=battleships-backend --env BROKER=default
