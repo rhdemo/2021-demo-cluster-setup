e72bc32e5595075266923a2c6185dbcf5ada759e
Evan Shortiss <evanshortiss@gmail.com>
2021-04-23 13:53:11 +0100

    fix: enable kafka forwarding fix: undeploy script fix: label for ui

--- frontend/undeploy.sh
+++ frontend/undeploy.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT=${PROJECT:-frontend}
 
 oc project ${PROJECT}
-oc process -p CLUSTER_NAME="${CLUSTER_NAME}" -p LOG_LEVEL='info' -f "${DIR}/common.yml" | oc delete -f -
+oc process -p CLUSTER_NAME="${CLUSTER_NAME}" -p ADMIN_PASSWORD="nope" -p LOG_LEVEL='info' -f "${DIR}/common.yml" | oc delete -f -
 oc process -f "${DIR}/admin.yml" | oc delete -f -
 oc process -f "${DIR}/game-server.yml" -p APPLICATION_NAME='game-server' -p NAMESPACE="${PROJECT}" | oc delete -f -
 oc process -f "${DIR}/game-ui.yml" -p APPLICATION_NAME='game' | oc delete -f -
