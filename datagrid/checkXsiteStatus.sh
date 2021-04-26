d64feaa13c9a2aef936c6e52516f3fe3c28c90a8
Ryan Emerson <remerson@redhat.com>
2021-04-26 14:32:04 +0100

    Datagrid. Add check for offline sites

--- datagrid/checkXsiteStatus.sh
+++ datagrid/checkXsiteStatus.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-set -e
 . ./common.sh
 
 aws
 echo "AWS"
-oc -n datagrid logs datagrid-0 | grep org.infinispan.XSITE
+oc -n datagrid logs datagrid-0 | grep -n org.infinispan.XSITE
+oc -n datagrid logs datagrid-0 | grep -n "is offline"
 
 gcp
 echo "GCP"
-oc -n datagrid logs datagrid-0 | grep org.infinispan.XSITE
+oc -n datagrid logs datagrid-0 | grep -n org.infinispan.XSITE
+oc -n datagrid logs datagrid-0 | grep -n "is offline"
 
 azure
 echo "AZURE"
-oc -n datagrid logs datagrid-0 | grep org.infinispan.XSITE
+oc -n datagrid logs datagrid-0 | grep -n org.infinispan.XSITE
+oc -n datagrid logs datagrid-0 | grep -n "is offline"
