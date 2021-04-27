6668f26d5461e91b29f90a3e2063100b666d3862
utherp0 <ian.lawson@redhat.com>
2021-04-27 12:10:05 +0100

    Added winscore to matchend

--- backend/scoring-service.sh
+++ backend/scoring-service.sh
@@ -1,4 +1,4 @@
 oc project battleships-scoring 2> /dev/null || oc new-project battleships-scoring
 
-oc apply -f backend/scoring-dc.yaml
+oc new-app quay.io/redhatdemo/2021-scoring-service --name="scoring-service" --as-deployment-config=true --env="QUARKUS_INFINISPAN_CLIENT_SERVER_LIST=datagrid.datagrid.svc.cluster.local:11222" --env="QUARKUS_HTTP_CORS=true"
 oc apply -f backend/scoring-route.yaml
